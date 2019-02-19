888f2c4e3b8e76304fc1585fd09e2164f9fcde97
Abdel <abdelwahaboufkir@pursuit.org>
2019-02-19 18:23:56 -0500

    initial commit

---
+++ precache-manifest.51776aa67d655bb41ed8cd2aacecf618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "599f20ad350135e56f08b5a0bf5e7f5c",
+    "url": "/static/media/pokemon-gotta-catch-em-all-lyrics-_audiotrimmer.com_.599f20ad.mp3"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec416684922005972e9e",
+    "url": "/static/js/main.ec416684.chunk.js"
+  },
+  {
+    "revision": "fde48b57843ad383c265",
+    "url": "/static/js/1.fde48b57.chunk.js"
+  },
+  {
+    "revision": "ec416684922005972e9e",
+    "url": "/static/css/main.1ae422f0.chunk.css"
+  },
+  {
+    "revision": "fde48b57843ad383c265",
+    "url": "/static/css/1.3167b3c6.chunk.css"
+  },
+  {
+    "revision": "30b81c5d8d7a26757bbc543fe0261b65",
+    "url": "/index.html"
+  }
+];
